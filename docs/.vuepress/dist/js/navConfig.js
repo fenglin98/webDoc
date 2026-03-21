bd7648d98120000c8c39b998ffb1ae43fac638cd
fenglin98 <2783041498@qq.com>
2026-03-21 23:28:40 +0800

    fix: 重新提交

--- docs/.vuepress/dist/js/navConfig.js
+++ docs/.vuepress/dist/js/navConfig.js
@@ -1,5 +1,109 @@
 module.exports = [
     { text: 'Home', link: '/' },
-    { text: 'External', link: 'https://google.com' },
-    { text: 'Until', link: 'https://google.com' },
+          // Ai 学习
+    {
+        text: 'AI学习',
+        items: [
+            {
+                text: "CaluedCode 学习",
+                  items: [
+                    {
+                        text: "安装claude code",
+                        link: "/views/aiStudyPage/claudeCode/claudeCodeInstall",
+                    },
+                ]
+            },
+        ],
+    },
+    // 工具
+    {
+        text: 'Util',
+        items: [
+            {
+                text: "JavaScript 工具封装",
+                link: "/views/utilsPage/jsUtils",
+            },
+            {
+                text: "CSS 样式重置",
+                link: "/views/utilsPage/cssStyleReset",
+            },
+            {
+                text: "资源记录",
+                link: "/views/utilsPage/resource",
+            },
+        ],
+    },
+    // 开发规范
+    {
+        text: '开发规范',
+        items: [
+            {
+                text: 'git 操作流程',
+                link: '/views/projectSpecification/gitRelated/gitProcess'
+            },
+            {
+                text: "编码规范",
+                items: [
+                    {
+                        text: "JavaScript 规范",
+                        link: "/views/projectSpecification/codeSpecification/javaScriptCode",
+                    },
+                    {
+                        text: "CSS 规范",
+                        link: "/views/projectSpecification/codeSpecification/cssCode",
+                    },
+                    {
+                        text: "HTML 规范",
+                        link: "/views/projectSpecification/codeSpecification/htmlCode",
+                    },
+                ],
+            },
+        ],
+    },
+    // 学习
+    {
+        text: '学习',
+        items: [
+            {
+                text: "Markdown 基础语法",
+                link: "/views/studyPage/markdownStudy",
+            },
+            {
+                text: "JavaScript 正则基本语法",
+                link: "/views/studyPage/regularExpression",
+            },
+            {
+                text: "Express学习文档",
+                items: [
+                    {
+                        text: "简单的express服务",
+                        link: "/views/studyPage/express/expressDemo",
+                    },
+                    {
+                        text: "express 中间件",
+                        link: "/views/studyPage/express/expressMiddleware",
+                    },
+                    {
+                        text: "express 路由中间件",
+                        link: "/views/studyPage/express/expressRoute",
+                    },
+                ]
+            },
+        ],
+    },
+    // 问题记录
+    {
+        text: '问题记录',
+        items: [
+            {
+                text: "JavaScript 相关",
+                link: "/views/ProblemRecord/JavaScriptAbout",
+            },
+            {
+                text: "Vue 相关",
+                link: "/views/ProblemRecord/VueAbout",
+            },
+        ],
+    },
+
 ]
---
+++ docs/.vuepress/dist/js/sidebarConfig.js
@@ -0,0 +1,124 @@
+module.exports = {
+        '/views/aiStudyPage': [
+        // Ai 学习
+        {
+            title: 'AI学习',
+            collapsable: true,
+            children: [
+                {
+                    title: "CaluedCode 学习",
+                    children: [
+                        {
+                            title: "安装claude code",
+                            path: "/views/aiStudyPage/claudeCode/claudeCodeInstall",
+                        },
+                    ]
+                },
+            ],
+        },
+    ],
+    // Utils 工具
+    '/views/utilsPage': [{
+        title: 'Util',
+        collapsable: true,
+        children: [
+            {
+                title: "JavaScript 工具封装",
+                path: "/views/utilsPage/jsUtils",
+            },
+            {
+                title: "CSS 样式重置",
+                path: "/views/utilsPage/cssStyleReset",
+            },
+            {
+                title: "资源记录",
+                path: "/views/utilsPage/resource",
+            },
+        ],
+    },
+    ],
+    // 开发规范 
+    '/views/projectSpecification': [
+        {
+            title: "git规范",
+            collapsable: true,
+            children: [
+                {
+                    title: "git 操作流程",
+                    path: "/views/projectSpecification/gitRelated/gitProcess",
+                }
+            ]
+        },
+        {
+            title: "代码规范",
+            collapsable: true,
+            children: [
+                {
+                    title: "JavaScript 规范",
+                    path: "/views/projectSpecification/codeSpecification/javaScriptCode",
+                },
+                {
+                    title: "CSS 规范",
+                    path: "/views/projectSpecification/codeSpecification/cssCode",
+                },
+                {
+                    title: "HTML 规范",
+                    path: "/views/projectSpecification/codeSpecification/htmlCode",
+                }
+            ]
+        },
+    ],
+    // 学习 
+    '/views/studyPage': [
+        {
+            title: '学习',
+            collapsable: true,
+            children: [
+                {
+                    title: "Markdown 基础语法",
+                    path: "/views/studyPage/markdownStudy",
+                },
+                {
+                    title: "JavaScript 正则基本语法",
+                    path: "/views/studyPage/regularExpression",
+                },
+                {
+                    title: "express学习文档",
+                    collapsable: true,
+                    children: [
+                        {
+                            title: "简单的express服务",
+                            path: "/views/studyPage/express/expressDemo",
+                        },
+                        {
+                            title: "express 中间件",
+                            path: "/views/studyPage/express/expressMiddleware",
+                        },
+                        {
+                            title: "express 路由中间件",
+                            path: "/views/studyPage/express/expressRoute",
+                        },
+                    ],
+                },
+            ],
+        }
+    ],
+    // 问题记录
+    '/views/ProblemRecord': [
+        {
+            title: '问题记录',
+            collapsable: true,
+            children: [
+                {
+                    title: "JavaScript 相关",
+                    path: "/views/ProblemRecord/JavaScriptAbout",
+                },
+                {
+                    title: "Vue 相关",
+                    path: "/views/ProblemRecord/VueAbout",
+                },
+            ],
+        }
+    ],
+
+}
